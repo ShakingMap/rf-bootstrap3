ce24cc295ac2e9932c8a95b90210d18b0ebd9b65
zhaoyao91 <zhaoyao91@163.com>
2016-05-19 17:41:48 +0800

    update array logic

--- src/Array.js
+++ src/Array.js
@@ -28,95 +28,59 @@ class Array extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            // move, add or remove
-            editMode: ''
+            updating: false
         }
     }
 
     render() {
         const {validationState, onInsert, onRemove, onMove, children, disabled} = this.props;
-        const {editMode} = this.state;
+        const {updating} = this.state;
 
         return <div className="panel panel-default">
             <div className="panel-body">
-                <div className="array-actions" style={{marginBottom: '10px'}}>
-                    {
-                        !disabled ?
-                            !children.length ?
-                                <button type="button" className="btn btn-primary" onClick={()=>onInsert(0)}>
-                                    <span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                                </button>
-                                :
-                                !editMode ? [
-                                    <button key={0} type="button" style={{marginRight: '10px'}}
-                                            className="btn btn-primary"
-                                            onClick={()=>this.setState({editMode: 'add'})}>
-                                        <span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                                    </button>,
-                                    <button key={1} type="button" style={{marginRight: '10px'}}
-                                            className="btn btn-primary"
-                                            onClick={()=>this.setState({editMode: 'remove'})}>
-                                        <span className="glyphicon glyphicon-minus" aria-hidden="true"></span>
-                                    </button>,
-                                    <button key={2} type="button" className="btn btn-primary"
-                                            onClick={()=>this.setState({editMode: 'move'})}>
-                                        <span className="glyphicon glyphicon-move" aria-hidden="true"></span>
-                                    </button>
-                                ]
-                                    :
+                {
+                    !disabled ?
+                        <div className="array-actions" style={{marginBottom: '10px'}}>
+                            {
+                                updating ?
                                     <button type="button" className="btn btn-primary"
-                                            onClick={()=>this.setState({editMode: null})}>
+                                            onClick={()=>this.setState({updating: false})}>
                                         <span className="glyphicon glyphicon-ok" aria-hidden="true"></span>
                                     </button>
-                            :
-                            null
-                    }
-                </div>
+                                    :
+                                    <button type="button" className="btn btn-primary"
+                                            onClick={()=>this.setState({updating: true})}>
+                                        <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                                    </button>
+                            }
+                        </div>
+                        : null
+                }
                 {children.map((child, index)=><div key={index}>
                     {
-                        !disabled && editMode === 'add' ?
-                            <button type="button" className="btn btn-primary" style={{marginBottom: '10px'}}
-                                    onClick={()=>onInsert(index)}>
-                                <span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                            </button> : null
+                        !disabled && updating ?
+                            <div className="array-item-actions" style={{marginBottom: '10px'}}>
+                                <button type="button" style={{marginRight: '10px'}} className="btn btn-primary"
+                                        onClick={()=>onInsert(index)}>
+                                    <span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                                </button>
+                                <button type="button" style={{marginRight: '10px'}} className="btn btn-primary"
+                                        onClick={()=>onRemove(index)}>
+                                    <span className="glyphicon glyphicon-minus" aria-hidden="true"></span>
+                                </button>
+                                <button type="button" style={{marginRight: '10px'}} className="btn btn-primary"
+                                        onClick={index > 0 ? ()=>onMove(index, index-1):null}>
+                                    <span className="glyphicon glyphicon-arrow-up" aria-hidden="true"></span>
+                                </button>
+                                <button type="button" className="btn btn-primary"
+                                        onClick={index < children.length -1? ()=>onMove(index, index+1): null}>
+                                    <span className="glyphicon glyphicon-arrow-down" aria-hidden="true"></span>
+                                </button>
+                            </div>
+                            : null
                     }
-                    <div style={{display: 'flex'}}>
-                        {
-                            !disabled && editMode === 'remove' ?
-                                <ArrayItemActions>
-                                    <button type="button" className="btn btn-danger"
-                                            onClick={()=>onRemove(index)}>
-                                        <span className="glyphicon glyphicon-minus" aria-hidden="true"></span>
-                                    </button>
-                                </ArrayItemActions> : null
-                        }
-                        {
-                            !disabled && editMode === 'move' ?
-                                <ArrayItemActions>
-                                    <button key={0} type="button" className="btn btn-primary"
-                                            style={{marginBottom: '10px'}}
-                                            onClick={index > 0 ? ()=>onMove(index, index-1):null}>
-                                                <span className="glyphicon glyphicon-arrow-up"
-                                                      aria-hidden="true"></span>
-                                    </button>
-                                    <button key={1} type="button" className="btn btn-primary"
-                                            onClick={index < children.length -1? ()=>onMove(index, index+1): null}>
-                                                <span className="glyphicon glyphicon-arrow-down"
-                                                      aria-hidden="true"></span>
-                                    </button>
-                                </ArrayItemActions> : null
-                        }
-                        <div style={{width: '100%'}}>
-                            {child}
-                        </div>
-                    </div>
+                    {child}
                 </div>)}
-                {
-                    !disabled && editMode === 'add' ? <button type="button" className="btn btn-primary"
-                                                              onClick={()=>onInsert(children.length)}>
-                        <span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                    </button> : null
-                }
             </div>
         </div>
     }
@@ -124,12 +88,5 @@ class Array extends React.Component {
 
 Array.propTypes = propTypes;
 Array.defaultProps = defaultProps;
-
-const ArrayItemActions = ({children}) => {
-    return <div {...{
-        className: 'array-item-actions',
-        style: {marginRight: '10px', display: 'flex', flexDirection: 'column'}
-    }}>{children}</div>
-};
 
 export default Array
